3292d9d94317b9ee5d2878fb32bceb9e196bc2ef
sHunjang <https://github.com/sHunjang/Clothing_Coordination.git>
2024-02-10 00:26:12 +0900

    feat : model Load & datasets folder rename

---
+++ yolov8-cls_Train.py
@@ -0,0 +1,12 @@
+from ultralytics import YOLO
+
+# Img Dataset
+Img_path = '/Users/seunghunjang/Desktop/Clothing_Classification/Dataset'
+Img_BGR_path = '/Users/seunghunjang/Desktop/Clothing_Classification/Dataset_BGR'
+
+# Load Model
+model = YOLO('yolov8m-cls.pt')
+
+
+# Train the model
+result = model.train(data=Img_path, epochs=30, imgsz=640)
