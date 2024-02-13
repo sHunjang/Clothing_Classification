993bd958068eed8ae05b1004cee8a00b970746e8
sHunjang <https://github.com/sHunjang/Clothing_Coordination.git>
2024-02-13 14:16:39 +0900

    feat : dataset renewal

--- model_predict.py
+++ model_predict.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load pretrained model
-model = YOLO('best.pt')
+model = YOLO('/opt/homebrew/runs/classify/train2/weights/best.pt')
 
 predic_BGR_img_path = '/Users/seunghunjang/Desktop/Clothing_Classification/Predict_dataset/BGR'
 predic_img_path = '/Users/seunghunjang/Desktop/Clothing_Classification/Predict_dataset/BGR_X'
--- model_validation.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load model
-model = YOLO() # Pre trained model ex)best.pt
-
-# Validate model
-metrics = model.val()
