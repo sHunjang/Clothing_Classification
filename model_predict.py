24b1dd3565a94c12adaff67dea7a3a6860518988
sHunjang <https://github.com/sHunjang/Clothing_Coordination.git>
2024-02-14 20:29:40 +0900

    feat : model train & predict

--- model_predict.py
+++ model_predict.py
@@ -1,10 +1,11 @@
 from ultralytics import YOLO
 
 # Load pretrained model
-model = YOLO('/opt/homebrew/runs/classify/train3/weights/best.pt')
+model = YOLO('/opt/homebrew/runs/classify/train18/weights/best.pt')
 
 predic_BGR_img_path = '/Users/seunghunjang/Desktop/Clothing_Classification/Predict_dataset/BGR'
 predic_img_path = '/Users/seunghunjang/Desktop/Clothing_Classification/Predict_dataset/BGR_X'
 
 # Predict image
-result = model.predict(source=predic_img_path, save=True)
+result = model.predict(source=predic_BGR_img_path, save=True)
+#result = model.predict('/path/to/image.PNG', save=True) # One image predict code
