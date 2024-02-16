2ac8078af9d264f0120bebfb64cbaa905e188ba9
sHunjang <https://github.com/sHunjang/Clothing_Coordination.git>
2024-02-16 15:46:13 +0900

    feat: dataset renewal & model train & predict

--- model_predict.py
+++ model_predict.py
@@ -1,11 +1,12 @@
 from ultralytics import YOLO
 
 # Load pretrained model
-model = YOLO('/opt/homebrew/runs/classify/train18/weights/best.pt')
+model = YOLO('/opt/homebrew/runs/classify/train15/weights/best.pt')
 
 predic_BGR_img_path = '/Users/seunghunjang/Desktop/Clothing_Classification/Predict_dataset/BGR'
 predic_img_path = '/Users/seunghunjang/Desktop/Clothing_Classification/Predict_dataset/BGR_X'
 
 # Predict image
-result = model.predict(source=predic_BGR_img_path, save=True)
+result = model.predict(source='/Users/seunghunjang/Desktop/Clothing_Classification/testset', save=True)
+
 #result = model.predict('/path/to/image.PNG', save=True) # One image predict code
