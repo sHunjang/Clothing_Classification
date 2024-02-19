a02c509bb7c3b221fd819d5e14fa822783526045
sHunjang <https://github.com/sHunjang/Clothing_Coordination.git>
2024-02-19 16:56:55 +0900

    feat : dataset name renewal

--- yolov8-cls_Train.py
+++ yolov8-cls_Train.py
@@ -4,9 +4,12 @@ from ultralytics import YOLO
 Img_path = '/Users/seunghunjang/Desktop/Clothing_Classification/Dataset'
 Img_BGR_path = '/Users/seunghunjang/Desktop/Clothing_Classification/Dataset_BGR'
 
+# Train Dir save Path
+path = '/Users/seunghunjang/Desktop/Clothing_Classification/Train'
+
 # Load Model
 model = YOLO('yolov8m-cls.pt')
 
 
 # Train the model
-result = model.train(data=Img_path, epochs=20, imgsz=640)
+result = model.train(data=Img_path, epochs=25, imgsz=640, save_dir=path)
