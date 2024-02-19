e931fee41a54821e449a310e8e0f84f4e19ffc1e
sHunjang <https://github.com/sHunjang/Clothing_Coordination.git>
2024-02-19 17:36:28 +0900

    feat : train & predict & model renewal

--- yolov8-cls_Train.py
+++ yolov8-cls_Train.py
@@ -8,8 +8,8 @@ Img_BGR_path = '/Users/seunghunjang/Desktop/Clothing_Classification/Dataset_BGR'
 path = '/Users/seunghunjang/Desktop/Clothing_Classification/Train'
 
 # Load Model
-model = YOLO('yolov8m-cls.pt')
+model = YOLO('yolov8s-cls.pt')
 
 
 # Train the model
-result = model.train(data=Img_path, epochs=25, imgsz=640, save_dir=path)
+result = model.train(data=Img_path, epochs=30, imgsz=640)
