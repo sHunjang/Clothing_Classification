f6e8f5e26cf2f531730a4ee26762fb69da1169f6
sHunjang <https://github.com/sHunjang/Clothing_Coordination.git>
2024-02-23 11:40:17 +0900

    feat : predict renewal

--- model_predict.py
+++ model_predict.py
@@ -7,6 +7,6 @@ predic_BGR_img_path = '/Users/seunghunjang/Desktop/Clothing_Classification/Predi
 predic_img_path = '/Users/seunghunjang/Desktop/Clothing_Classification/Predict_dataset/BGR_X'
 
 # Predict image
-result = model.predict(source=predic_img_path, save=True)
+result = model.predict(source=predic_img_path, save=True, save_txt=True)
 
 #result = model.predict('/path/to/image.PNG', save=True) # One image predict code
