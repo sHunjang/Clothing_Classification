aa1437bd25ed9c7847ad155aab429e65fc54f378
sHunjang <https://github.com/sHunjang/Clothing_Coordination.git>
2024-03-12 20:02:41 +0900

    feat : research renewal & add result txt file

--- model_predict.py
+++ model_predict.py
@@ -1,10 +1,10 @@
 from ultralytics import YOLO
 
 # Load pretrained model
-model = YOLO('/opt/homebrew/runs/classify/train9/weights/best.pt')
+model = YOLO('/opt/homebrew/runs/classify/train18/weights/best.pt')
 
 
-predic_BGR_img_path = '/Users/seunghunjang/Desktop/Clothing_Classification/Predict_dataset/BGR'
+#predic_BGR_img_path = '/Users/seunghunjang/Desktop/Clothing_Classification/Predict_dataset/BGR'
 predic_img_path = '/Users/seunghunjang/Desktop/Clothing_Classification/Predict_dataset/BGR_X'
 
 # Predict image
--- yolov8-cls_Train.py
+++ yolov8-cls_Train.py
@@ -8,8 +8,8 @@ Img_path = '/Users/seunghunjang/Desktop/Clothing_Classification/Dataset'
 path = '/Users/seunghunjang/Desktop/Clothing_Classification/Train'
 
 # Load Model
-model = YOLO('yolov8m-cls.pt')
+model = YOLO('yolov8s-cls.pt')
 
 
 # Train the model
-result = model.train(data=Img_path, epochs=25, imgsz=640)
+result = model.train(data=Img_path, epochs=30, imgsz=640)
