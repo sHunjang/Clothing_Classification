fc64c8d1e06a77f1bdfc1ba7ae4988a34feb8c0a
sHunjang <https://github.com/sHunjang/Clothing_Coordination.git>
2024-03-13 11:48:11 +0900

    feat : train cache=True & re train/predict

--- yolov8-cls_Train.py
+++ yolov8-cls_Train.py
@@ -5,11 +5,11 @@ Img_path = '/Users/seunghunjang/Desktop/Clothing_Classification/Dataset'
 #Img_BGR_path = '/Users/seunghunjang/Desktop/Clothing_Classification/Dataset_BGR'
 
 # Train Dir save Path
-path = '/Users/seunghunjang/Desktop/Clothing_Classification/Train'
+# path = '/Users/seunghunjang/Desktop/Clothing_Classification/Train'
 
 # Load Model
 model = YOLO('yolov8s-cls.pt')
 
 
 # Train the model
-result = model.train(data=Img_path, epochs=30, imgsz=640)
+result = model.train(data=Img_path, epochs=20, imgsz=640, cache=True)
