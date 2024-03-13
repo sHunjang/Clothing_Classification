7011871585139a35744bba5c97c14e13dd6c9f8a
sHunjang <https://github.com/sHunjang/Clothing_Coordination.git>
2024-03-13 13:09:57 +0900

    feat : imgsz -> 480

--- yolov8-cls_Train.py
+++ yolov8-cls_Train.py
@@ -4,12 +4,10 @@ from ultralytics import YOLO
 Img_path = '/Users/seunghunjang/Desktop/Clothing_Classification/Dataset'
 #Img_BGR_path = '/Users/seunghunjang/Desktop/Clothing_Classification/Dataset_BGR'
 
-# Train Dir save Path
-# path = '/Users/seunghunjang/Desktop/Clothing_Classification/Train'
 
 # Load Model
 model = YOLO('yolov8s-cls.pt')
 
 
 # Train the model
-result = model.train(data=Img_path, epochs=20, imgsz=640, cache=True)
+result = model.train(data=Img_path, epochs=10, imgsz=480, cache=True, save_dir=path)
